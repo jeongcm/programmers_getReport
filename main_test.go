27075490c05d16b53f767f7d849c11fc564cdd3f
jcm <jeongcm0101@gmail.com>
2022-07-05 16:42:04 +0900

    test case 추가

--- main_test.go
+++ main_test.go
@@ -18,3 +18,22 @@ func TestMergeReport(t *testing.T) {
 	r := mergeReport(report)
 	t.Log(r)
 }
+
+func TestSolution(t *testing.T) {
+	for _, value := range []struct {
+		list   []string
+		report []string
+		count  int
+		result []int
+	}{
+		{
+			[]string{"con", "ryan"},
+			[]string{"con ryan", "ryan frodo"},
+			2,
+			[]int{0, 0},
+		},
+	} {
+		r := solution(value.list, value.report, value.count)
+		t.Log(r)
+	}
+}
